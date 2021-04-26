ec59007bf286bc9f2ec751d20d5e2f547d7bd3e3
devclariter <83215292+devclariter@users.noreply.github.com>
2021-04-26 19:56:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
-import test
+#import test
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Help me, No! "+ str(3+test.test_function(3))
+    return "Help me, No! "#+ str(3+test.test_function(3))
